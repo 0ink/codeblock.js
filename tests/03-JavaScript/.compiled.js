eefb6d327af3c4d4f3954f942c6238e19f8ebe76
Christoph Dorn <christoph@christophdorn.com>
2017-04-15 12:08:56 -0700

    wip

--- tests/03-JavaScript/.compiled.js
+++ tests/03-JavaScript/.compiled.js
@@ -1,10 +1,11 @@
 {
     "data": {
-        "message": "Hello World"
+        "message": "Hello World",
+        "chars": "`-=[]\\;',./~_+{}|:\"<>"
     },
     "main": {
         ".@": "github.com~0ink~codeblock/codeblock:Codeblock",
-        "_code": "\nconsole.log([\"Hello World\"].join(\"\\n\"));\nconsole.log(data.message);\n\nreturn data.message;",
+        "_code": "\nconsole.log([\"Hello World\"].join(\"\\n\"));\nconsole.log(data.message);\n\nconsole.log(\"`-=[]\\;',./~_+{}|:\\\"<>\");\nconsole.log('`-=[]\\;',./~_+{}|:\\\"<>');\nconsole.log(data.chars);\n\nreturn data.message;",
         "_format": "javascript",
         "_args": [
             "data"
--- tests/03-JavaScript/.parsed.js
+++ tests/03-JavaScript/.parsed.js
@@ -1,10 +1,11 @@
 {
     "data": {
-        "message": "Hello World"
+        "message": "Hello World",
+        "chars": "`-=[]\\;',./~_+{}|:\"<>"
     },
     "main": {
         ".@": "github.com~0ink~codeblock/codeblock:Codeblock",
-        "_code": "\nconsole.log([\"%%%data.message%%%\"].join(\"\\n\"));\nconsole.log(data.message);\n\nreturn data.message;",
+        "_code": "\nconsole.log([\"%%%data.message%%%\"].join(\"\\n\"));\nconsole.log(data.message);\n\nconsole.log(\"%%%data.chars%%%\");\nconsole.log('%%%data.chars%%%');\nconsole.log(data.chars);\n\nreturn data.message;",
         "_format": "javascript",
         "_args": [
             "data"
